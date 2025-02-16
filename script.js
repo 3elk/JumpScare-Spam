326a91551ab46a76eaabe5620022a344fed3c2d6
elk <elkkerson@gmail.com>
2025-02-16 22:52:41 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var released = false
+
+document.getElementById("x").addEventListener("click",spam)
+
+spam()
+
+setInterval(death,1)
+
+function spam(){
+
+    released=true
+
+}
+
+function death(){
+
+if(released===true){
+
+    window.open("jumpscare.mp4", "_blank");
+
+}
+
+}
